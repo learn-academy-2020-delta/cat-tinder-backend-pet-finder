fbb29352e69f9a9a04aedb1fd483e9c3094f6253
drewbrad4 <drewbrad4@gmail.com>
2020-10-23 12:07:57 -0700

    stop seed file from generating emtpy files

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,36 +21,63 @@ users = [
 
   users.each do |attributes|
       User.create attributes
-      puts `creating pet #{attributes}`
   end
 
-  pets = [
-{
-id: 1,
-name: 'Mittens',
-age: 5,
-species: 'Cat',
-breed: 'Short-Haired',
-enjoys: 'sunshine and warm spots'
-},
-{
-id: 2,
-name: 'Raisins',
-age: 4,
-species: 'Cat',
-breed: 'Flat-Faced',
-enjoys: 'scaring the dogs'
-},
-{
-id: 3,
-name: 'Toast',
-age: 1,
-species: 'Dog',
-breed: 'Poodle',
-enjoys: 'getting all the attention'
-}
-]
-pets.each do |attributes|
-    Pet.create attributes
-    puts `creating pet #{attributes}`
-end
+pets = [
+    {
+        id: 1,
+        name: 'Mittens',
+        age: 5,
+        species: 'Cat',
+        breed: 'Short-Haired',
+        enjoys: 'sunshine and warm spots'
+    },
+    {
+        id: 2,
+        name: 'Raisins',
+        age: 4,
+        species: 'Cat',
+        breed: 'Flat-Faced',
+        enjoys: 'scaring the dogs'
+    },
+    {
+
+        id: 3,
+        name: 'Toast',
+        age: 1,
+        species: 'Dog',
+        breed: 'Poodle',
+        enjoys: 'getting all the attention'
+    },
+    {
+
+        id: 4,
+        name: "Benny",
+        age: 10,
+        species: "Dog",
+        breed: "Husky",
+        enjoys: "scaring the cats"
+    },
+    {
+
+        id: 5,
+        name: "Tash",
+        age: 1,
+        species: "Cat",
+        breed: "Calico",
+        enjoys: "purrin and stuff"
+    },
+    {
+
+        id: 6,
+        name: "Steven",
+        age: 1,
+        species: "Dog",
+        breed: "Chihuahua",
+        enjoys: "Seeing out of one eye"
+    }
+    ]   
+    
+    pets.each do |attributes|
+        Pet.create attributes
+    end
